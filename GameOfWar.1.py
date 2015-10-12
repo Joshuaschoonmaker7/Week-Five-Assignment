d5fc33fc057ea18833aa401fe2925a4e5d3580c0
joshuaschoonmaker7 <joshuaschoonmaker7@gmail.com>
2015-10-12 03:25:56 +0000

    Added Answers.txt, more changes to come by Wednesday.

---
+++ GameOfWar.1.py
@@ -0,0 +1,77 @@
+# Joshua Schoonmaker
+# Intro Computer Sciences
+# GameOfWar.1.py
+# This program plays version 1 of the card game WAR
+
+
+import random	
+
+def main():
+	"""
+	Deck, PlayerAHand and PlayerBHand are all lists
+	"""
+	
+	Deck = []
+	PlayerAHand = []
+	PlayerBHand = []
+	gameCounter = 0
+
+	# Create deck.  Cards are represented by an integer value
+	for i in range(52):
+		Deck.append(i)
+	
+	# Shuffle the deck
+	random.shuffle(Deck)
+	
+	# Deal 1/2 the cards to each player
+	for x in range(26):
+		PlayerAHand.append(Deck.pop())
+		PlayerBHand.append(Deck.pop())
+	
+	# Main Gameplay
+		
+	while len(PlayerAHand) > 0 and len(PlayerBHand) > 0:
+		gameCounter = gameCounter + 1
+		PlayerAHand, PlayerBHand = playRound(PlayerAHand, PlayerBHand)
+		if gameCounter == 1000:
+			break
+	
+	# End of game
+	
+	print("There were ", gameCounter, " rounds played:")
+	print()
+	print("Player A had ",len(PlayerAHand), "cards, and player B had ",len(PlayerBHand), "cards")
+	
+def playRound(PlayerAHand, PlayerBHand):
+	
+	PlayerACard = PlayerAHand.pop()
+	PlayerBCard = PlayerBHand.pop()
+	
+	if getRank(PlayerACard) == getRank(PlayerBCard):
+	
+		WAR(PlayerAHand, PlayerBHand)
+	
+	else:
+		
+		if PlayerACard > PlayerBCard:
+			PlayerAHand.insert(0,PlayerACard)
+			PlayerAHand.insert(0,PlayerBCard)
+
+		else:
+			PlayerBHand.insert(0,PlayerACard)
+			PlayerBHand.insert(0,PlayerBCard)
+	
+	return PlayerAHand, PlayerBHand
+
+
+def WAR(PlayerAHand, PlayerBHand):
+
+	return PlayerAHand, PlayerBHand
+
+	
+def getRank(anyCard):
+	return anyCard % 13
+
+
+if __name__ == '__main__':
+	main()
